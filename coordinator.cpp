be381282c8432b86bc5ef4eb42343af478070e11
kalexoudis <k.alexoudis@tum.de>
2022-11-21 22:00:42 +0100

    setup socket connection between coordinator and worker

--- coordinator.cpp
+++ coordinator.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std::literals;
 
@@ -10,18 +14,93 @@ using namespace std::literals;
 /// Example:
 ///    ./coordinator http://example.org/filelist.csv 4242
 int main(int argc, char* argv[]) {
+   std::cout << "Coordinator started." << std::endl;
+
    if (argc != 3) {
       std::cerr << "Usage: " << argv[0] << " <URL to csv list> <listen port>" << std::endl;
       return 1;
    }
 
-   // TODO:
-   //    1. Allow workers to connect
-   //       socket(), bind(), listen(), accept(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. Distribute the following work among workers
-   //       send() them some work
-   //    3. Collect all results
-   //       recv() the results
+   // 1. Allow workers to connect socket(), bind(), listen(), accept(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
+   // 1.1. getaddrinfo()
+   //const size_t bufferSize = 1024;
+   const char* portNumber = argv[2];
+   const int backlog = 1;
+   int serverSocket;
+   struct addrinfo hints;
+   memset(&hints, 0, sizeof(struct addrinfo));
+   struct addrinfo* results;
+   struct addrinfo* record;
+   hints.ai_family = AF_INET;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_protocol = IPPROTO_TCP;
+
+   if ((getaddrinfo(NULL, portNumber, &hints, &results)) != 0) { // Translate address
+      std::perror("Failed to translate coordinator socket.");
+      exit(EXIT_FAILURE);
+   }
+   std::cout << "Coordinator socket translated." << std::endl;
+
+   // 1.2. socket()
+   for (record = results; record != NULL; record = record->ai_next) {
+      serverSocket = socket(record->ai_family, record->ai_socktype, record->ai_protocol); // Attempt to create socket from information provided in current record
+      // Skip current iteration in the loop if socket creation failss
+      if (serverSocket == -1) {
+         continue;
+      }
+      int enable = 1;
+      setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)); // Configure server socket to reuse same port. Helpful if the server needs to restart
+      // 1.3. bind()
+      if (bind(serverSocket, record->ai_addr, record->ai_addrlen) == 0) {
+         break;
+      }
+      // Close the socket if socket creation is successful but binding is unsuccessful
+      close(serverSocket);
+   }
+
+   if (record == NULL) { // record will iterate to NULL if the above loop encounters no success
+      std::perror("Failed to create or connect worker socket.");
+      exit(EXIT_FAILURE);
+   }
+   freeaddrinfo(results);
+   std::cout << "Server socket created and bound." << std::endl;
+
+   // 1.4. listen()
+   if (listen(serverSocket, backlog) == -1) { // Start server socket listen
+      std::perror("Failed to start server socket listen.");
+      exit(EXIT_FAILURE);
+   }
+   std::cout << "Server started listening." << std::endl;
+
+   // 2. Distribute the following work among workers send() them some work
+   while (1) {
+      std::cout << "Coordinator still running" << std::endl;
+      // 2.1. accept()
+      int clientSocket;
+      struct sockaddr clientAddress;
+      socklen_t clientAddressLength = sizeof(clientAddress);
+
+      if ((clientSocket = accept(serverSocket, &clientAddress, &clientAddressLength)) < 0) {
+         std::perror("Failed to accept client socket.");
+         exit(EXIT_FAILURE);
+      }
+      std::cout << "Client socket accepted." << std::endl;
+
+      // 2.2. send()
+      std::string message = "Hello from the coordinator";
+      if (send(clientSocket, message.c_str(), message.length(), 0) < 0) {
+         std::perror("Failed to send message to client.");
+         exit(EXIT_FAILURE);
+      }
+
+      // 3. Collect all results recv() the results
+      char buffer[1024];
+      if (recv(clientSocket, buffer, sizeof(buffer), 0) == -1) { // Read message
+         perror("Failed to receive message.");
+         exit(EXIT_FAILURE);
+      }
+      std::cout << "Message received: " << buffer << std::endl;
+   }
    // Hint: Think about how you track which worker got what work
 
    auto curlSetup = CurlGlobalSetup();
--- worker.cpp
+++ worker.cpp
@@ -1,24 +1,92 @@
+#include <cstdio>
 #include <iostream>
+#include <string>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 /// Worker process that receives a list of URLs and reports the result
 /// Example:
 ///    ./worker localhost 4242
 /// The worker then contacts the leader process on "localhost" port "4242" for work
 int main(int argc, char* argv[]) {
+   std::cout << "Worker started!" << std::endl;
+
    if (argc != 3) {
       std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
       return 1;
    }
 
+   // 1. connect to coordinator specified by host and port getaddrinfo(), connect(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
+   // 1.1. getaddrinfo()
+   const size_t bufferSize = 1024;
+   const char* hostName = argv[1];
+   const char* portNumber = argv[2];
+   int clientSocket;
+   struct addrinfo hints;
+   memset(&hints, 0, sizeof(struct addrinfo));
+   struct addrinfo* results;
+   struct addrinfo* record;
+   hints.ai_family = AF_INET;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_protocol = IPPROTO_TCP;
+
+   if ((getaddrinfo(hostName, portNumber, &hints, &results)) != 0) { // Translate address
+      std::perror("Failed to translate worker socket.");
+      exit(EXIT_FAILURE);
+   }
+   std::cout << "Worker socket translated." << std::endl;
+
+   // 1.2. connect()
+   for (record = results; record != NULL; record = record->ai_next) { // Iterate through every record in results
+      clientSocket = socket(record->ai_family, record->ai_socktype, record->ai_protocol); // Attempt to create socket from information provided in current record
+      // Skip current iteration in the loop if socket creation fails
+      if (clientSocket == -1) {
+         continue;
+      }
+      // Attempt to connect socket after its successful creation. If successful, break out of the loop
+      if (connect(clientSocket, record->ai_addr, record->ai_addrlen) != -1) {
+         break;
+      }
+      // Close the socket if socket creation is successful but connection is unsuccessful
+      close(clientSocket);
+   }
+   // record will iterate to NULL if the above loop encounters no success
+   if (record == NULL) {
+      std::perror("Failed to create or connect worker socket.");
+      exit(EXIT_FAILURE);
+   }
+   freeaddrinfo(results);
+   std::cout << "Worker socket created and connected." << std::endl;
+
+   // 2. receive work from coordinator recv(), matching the coordinator's send() work
+   // 2.1. recv()
+   char buffer[bufferSize];
+
+   if (recv(clientSocket, buffer, sizeof(buffer), 0) == -1) { // Read message
+      perror("Failed to receive message.");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("---------- RECEIVED MESSAGE ----------\n%s\n----------- END OF MESSAGE -----------\n", buffer);
+
+   //    3. process work see coordinator.cpp
+   //    3.1. process work
+   const char* message = "Hello from worker!";
+
+   // 4. report result send(), matching the coordinator's recv()
+   if (send(clientSocket, message, strlen(message), 0) == -1) { // Send data
+      std::perror("Failed to perform cognitive recalibration."); // Error message for when send() fails
+      exit(EXIT_FAILURE);
+   } else {
+      std::cout << "Subliminal message has been planted." << std::endl;
+   }
+
+   // 6. close connection close()
+   close(clientSocket);
+   std::cout << "Worker socket closed." << std::endl;
+
    // TODO:
-   //    1. connect to coordinator specified by host and port
-   //       getaddrinfo(), connect(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. receive work from coordinator
-   //       recv(), matching the coordinator's send() work
-   //    3. process work
-   //       see coordinator.cpp
-   //    4. report result
-   //       send(), matching the coordinator's recv()
    //    5. repeat
 
    return 0;
